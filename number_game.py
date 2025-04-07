ac9c22347042c13e86423992189260560676eb2a
ka6865 <ka6865@naver.com>
2025-04-07 22:32:18 +0900

    정답 출력 문구에 시도 횟수 추가

--- number_game.py
+++ number_game.py
@@ -1,16 +1,16 @@
 import random
 
 answer = random.randint(1, 100)
-
+attempts = 0  # 시도 횟수를 저장하는 변수
 print("1부터 100 사이 숫자를 맞춰보세요!")
 
 while True:
     guess = int(input("숫자를 입력하세요: "))
-
+    attempts += 1  # 시도할 때마다 +1
     if guess < answer:
         print("UP")
     elif guess > answer:
         print("DOWN")
     else:
-        print("정답입니다!")
+        print(f"정답입니다! {attempts}번 만에 맞췄어요!")
         break
